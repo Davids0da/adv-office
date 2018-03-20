c869780e99350e7f5b3bab7acfa159c0bbeb6fe0
davidsodaa <sodakolor@gmail.com>
2018-03-20 21:35:34 +0100

    Changes

--- public/layout/scripts/jquery.backtotop.js
+++ public/layout/scripts/jquery.backtotop.js
@@ -19,3 +19,44 @@ jQuery(window).scroll(function () {
         jQuery("#backtotop").removeClass("visible");
     }
 });
+
+$(".box1").hover(
+    function () {
+        $('.img-1').addClass("hide");
+        $('.img-1-blue').removeClass("hide");
+    }, 
+    function () { 
+        $('.img-1').removeClass("hide");
+        $('.img-1-blue').addClass("hide");
+    }
+);
+$(".box2").hover(
+    function () {
+        $('.img-2').addClass("hide");
+        $('.img-2-blue').removeClass("hide");
+    }, 
+    function () { 
+        $('.img-2').removeClass("hide");
+        $('.img-2-blue').addClass("hide");
+    }
+);
+$(".box3").hover(
+    function () {
+        $('.img-3').addClass("hide");
+        $('.img-3-blue').removeClass("hide");
+    }, 
+    function () { 
+        $('.img-3').removeClass("hide");
+        $('.img-3-blue').addClass("hide");
+    }
+);
+$(".box4").hover(
+    function () {
+        $('.img-4').addClass("hide");
+        $('.img-4-blue').removeClass("hide");
+    }, 
+    function () { 
+        $('.img-4').removeClass("hide");
+        $('.img-4-blue').addClass("hide");
+    }
+);
